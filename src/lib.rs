fb69e7b700b4af3ff5b03b246aea6c5f4fedcd8d
Chaya <cheyenne.xyn@gmail.com>
2024-08-15 16:44:41 +0100

    trial

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+use wasm_bindgen::prelude::*;
+
+// exposing `summing_up_squares` function to JavaScript
+#[wasm_bindgen]
+pub fn summing_up_squares(n: u32) -> u32 {
+    (1..=n).map(|x| x * x).sum()
+}
+
+#[wasm_bindgen]
+pub fn test_summing_up_squares() -> bool {
+    summing_up_squares(7) == 140
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_summing_up_squares() {
+        assert_eq!(summing_up_squares(7), 140);
+    }
+}
